ccb7ad047c52d313c108b56cc83d853e276e86d3
Judongsung <57513715+Judongsung@users.noreply.github.com>
2026-03-19 22:34:59 +0900

    build: 서비스용 url 반영

--- Client/wwwroot/config.js
+++ Client/wwwroot/config.js
@@ -1,3 +1,7 @@
-﻿const CONFIG = {
-    SERVER_URL: "http://localhost:5149/omokHub"
+﻿const isProduction = window.location.hostname.includes("pages.dev");
+
+const CONFIG = {
+    SERVER_URL: isProduction
+        ? "https://eerie-ricki-judongsung-275ef74a.koyeb.app/omokHub"
+        : "http://localhost:5149/omokHub"
 }
